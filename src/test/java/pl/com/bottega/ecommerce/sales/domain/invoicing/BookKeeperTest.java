915a4cf4f1a99d97aebbaef0bfbc2049de731f87
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-04-15 17:51:19 +0200

    isolation add invoiceRequestWithTwoItemsShouldCallCalculateTaxTwice test

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -14,14 +14,17 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookKeeperTest {
 
     @Mock
     private TaxPolicy taxPolicy;
+    @Mock
+    private ProductData product;
+
     private ClientData clientData;
     private InvoiceRequest invoiceRequest;
     private InvoiceFactory invoiceFactory;
@@ -32,26 +35,41 @@ public class BookKeeperTest {
         clientData = new ClientData(Id.generate(), insignificantName());
         invoiceRequest = new InvoiceRequest(clientData);
         invoiceFactory = new InvoiceFactory();
+        keeper = new BookKeeper(invoiceFactory);
     }
 
     @Test
     public void invoiceRequestWithEmptyItemShouldReturnEmptyInvoice() {
-        keeper = new BookKeeper(invoiceFactory);
         Invoice invoice = keeper.issuance(invoiceRequest, taxPolicy);
         Assert.assertThat(invoice.getItems().isEmpty(), is(equalTo(invoiceRequest.getItems().isEmpty())));
     }
 
     @Test
     public void invoiceRequestWithOneItemShouldReturnInvoiceWithOneItem() {
-        ProductData product = mock(ProductData.class);
+
         when(product.getType()).thenReturn(ProductType.DRUG);
         RequestItem requestItem = new RequestItem(product, 1, new Money(100));
         invoiceRequest.add(requestItem);
 
-        when(taxPolicy.calculateTax(ProductType.DRUG, new Money(100))).thenReturn(new Tax(new Money(10), "10%"));
-        keeper = new BookKeeper(invoiceFactory);
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(new Tax(new Money(10), "10%"));
         Invoice returnInvoice = keeper.issuance(invoiceRequest, taxPolicy);
         Assert.assertThat(returnInvoice.getItems().size(), is(1));
+    }
+
+    @Test
+    public void invoiceRequestWithTwoItemsShouldCallCalculateTaxTwice() {
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(new Tax(new Money(10), "10%"));
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(new Tax(new Money(10), "10%"));
+
+        when(product.getType()).thenReturn(ProductType.DRUG);
+        RequestItem requestItem = new RequestItem(product, 1, new Money(100));
+        invoiceRequest.add(requestItem);
+
+        requestItem = new RequestItem(product, 1, new Money(200));
+        invoiceRequest.add(requestItem);
+
+        keeper.issuance(invoiceRequest, taxPolicy);
+        verify(taxPolicy, times(2)).calculateTax(any(), any());
     }
 
     private String insignificantName() {
