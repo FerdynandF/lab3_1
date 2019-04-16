8367ed6a518a19451fda2731e898020ca1903fff
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-04-16 13:44:21 +0200

    1.2 add suggestionServiceMethodShouldNotBeCalled test

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -40,7 +40,7 @@ public class AddProductCommandHandlerTest {
 
     @Before
     public void initialize() {
-        command = new AddProductCommand(Id.generate(), new Id("1"), 1);
+        command = new AddProductCommand(Id.generate(), Id.generate(), 1);
         Client client = new Client();
         when(product.isAvailable()).thenReturn(true);
         when(reservationRepository.load(any())).thenReturn(reservation);
@@ -52,15 +52,26 @@ public class AddProductCommandHandlerTest {
 
     @Test
     public void reservationRepositoryLoadShouldBeCalledOnce() {
-        command = new AddProductCommand(Id.generate(), Id.generate(), 1);
         addProductCommandHandler.handle(command);
         verify(reservationRepository, times(1)).load(any());
     }
 
     @Test
     public void productRepositoryLoadShouldBeCalledOnce() {
-        command = new AddProductCommand(Id.generate(), Id.generate(), 1);
         addProductCommandHandler.handle(command);
         verify(productRepository, times(1)).load(any(Id.class));
     }
+
+    @Test
+    public void reservationRepositorySaveShouldBeCalledOnce() {
+        addProductCommandHandler.handle(command);
+        verify(reservationRepository, times(1)).save(any(Reservation.class));
+    }
+
+    @Test
+    public void suggestionServiceMethodShouldNotBeCalled() {
+        addProductCommandHandler.handle(command);
+        verify(suggestionService, never()).suggestEquivalent(any(Product.class), any(Client.class));
+    }
+
 }
